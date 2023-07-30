97c9025b78e056395d9d259b8fce21bffa38be18
Fu Zhengyi <tsingyat@outlook.com>
2023-07-30 13:52:50 +0800

    rewrite example using C++20 coroutine

--- main.cpp
+++ main.cpp
@@ -1,26 +1,43 @@
 #include <inotify-asio/inotify.hpp>
+#include <boost/asio/co_spawn.hpp>
+#include <boost/asio/use_awaitable.hpp>
+#include <boost/asio/detached.hpp>
+#include <span>
 #include <iostream>
 
-void watch_handler(inotify_asio::inotify &ino, boost::system::error_code ec, inotify_asio::event const &ev)
+using boost::asio::use_awaitable;
+using boost::asio::use_awaitable_t;
+using inotify = use_awaitable_t<>::as_default_on_t<inotify_asio::inotify>;
+
+boost::asio::awaitable<void> 
+watch(boost::asio::io_context &ioc, std::span<const char *> files)
 {
-    if (ec)
+    try
     {
-        std::cerr << "async watch: " << ec.message() << std::endl;
-        return;
+        inotify ino(ioc);
+
+        for (auto file : files)
+        {
+            auto item = ino.add(file,IN_CLOSE | IN_OPEN | IN_CREATE | IN_ACCESS | IN_MOVE | IN_DELETE);
+            item.forget();
+        }
+
+        for (;;)
+        {
+            auto ev = co_await ino.async_watch();
+            std::cout << "wd: " << ev.wd() << "\n"
+                        << "mask: " << ev.mask() << "\n"
+                        << "cookie: " << ev.cookie() << "\n"
+                        << "name: " << ev.name() << "\n";
+        }
     }
-
-    std::cout << "wd: " << ev.wd() << "\n"
-                << "mask: " << ev.mask() << "\n"
-                << "cookie: " << ev.cookie() << "\n"
-                << "name: " << ev.name() << "\n";
-
-    ino.async_watch([&ino](auto ec, auto ev)
+    catch (std::exception &exc)
     {
-        watch_handler(ino, ec, ev);
-    });
+        std::cerr << exc.what() << std::endl;
+    }
 }
 
-int main(int argc, char **argv)
+int main(int argc, const char **argv)
 {
     if (argc < 2)
     {
@@ -30,20 +47,7 @@ int main(int argc, char **argv)
 
     try {
         boost::asio::io_context ioc;
-
-        inotify_asio::inotify ino(ioc);
-
-        for (int i = 1; i < argc; ++i)
-        {
-            auto item = ino.add(argv[i], IN_CLOSE | IN_OPEN | IN_CREATE | IN_ACCESS | IN_MOVE | IN_DELETE);
-            item.forget();
-        }
-
-        ino.async_watch([&ino](auto ec, auto ev)
-        {
-            watch_handler(ino, ec, ev);
-        });
-
+        boost::asio::co_spawn(ioc, watch(ioc, std::span<const char *>(argv + 1, argc - 1)), boost::asio::detached);
         ioc.run();
     }
     catch (std::exception const &exc)
